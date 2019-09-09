2c0e90668cad110e456bf14978c9cd573f0e23f4
netsolin admin <netsoladmin@NETSOL.LOCAL>
2019-09-09 11:09:42 -0500

    Version 2.9.7

--- src/app/pages/support/support.page.ts
+++ src/app/pages/support/support.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-support',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupportPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
   }
